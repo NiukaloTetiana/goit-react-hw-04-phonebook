5f2abfc73abaabfc101728eca8c523785fbe0d55
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-02-01 00:11:29 +0200

    change path of files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
-import { theme } from 'components/styles/theme';
-import { GlobalStyle } from 'components/styles/CreateGlobalStyle';
+import { theme } from 'styles/theme';
+import { GlobalStyle } from 'styles/CreateGlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
